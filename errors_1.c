bcfce789b9133c4845146a996c3c3615ee73ea6a
gedithecoder <117764141+gedithecoder@users.noreply.github.com>
2023-03-23 01:33:21 +0300

    Update errors_1.c

--- errors_1.c
+++ errors_1.c
@@ -13,7 +13,7 @@ int no_int_error(unsigned int line_number);
  */
 int usage_error(void)
 {
-	fprintf(stderr, "usage: monty file\n");
+	fprintf(stderr, "USAGE: monty file\n");
 	return (EXIT_FAILURE);
 }
 
@@ -24,7 +24,7 @@ int usage_error(void)
  */
 int malloc_error(void)
 {
-	fprintf(stderr, "Error: malloc fallen\n");
+	fprintf(stderr, "Error: malloc failed\n");
 	return (EXIT_FAILURE);
 }
 
@@ -37,7 +37,7 @@ int malloc_error(void)
  */
 int f_open_error(char *filename)
 {
-	fprintf(stderr, "Error: Can't open file %s\n", filename);
+  	fprintf(stderr, "Error: Can't open file %s\n", filename);
 	return (EXIT_FAILURE);
 }
 /**
@@ -49,7 +49,7 @@ int f_open_error(char *filename)
  */
 int unknown_op_error(char *opcode, unsigned int line_number)
 {
-	fprintf(stderr, "L%u: unkown instruction %s\n",
+	fprintf(stderr, "L%u: unknown instruction %s\n",
 		line_number, opcode);
 	return (EXIT_FAILURE);
 }
