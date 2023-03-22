ce05f5d8aaf9213ebcdc4c054d6227f34450357e
gedithecoder <117764141+gedithecoder@users.noreply.github.com>
2023-03-23 02:38:40 +0300

    Update monty_funcs_4.c

--- monty_funcs_4.c
+++ monty_funcs_4.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-void monty_rol1(stack_t **stack, unsigned int line_number);
+void monty_rotl(stack_t **stack, unsigned int line_number);
 void monty_rotr(stack_t **stack, unsigned int line_number);
 void monty_stack(stack_t **stack, unsigned int line_number);
 void monty_queue(stack_t **stack, unsigned int line_number);
@@ -10,7 +10,7 @@ void monty_queue(stack_t **stack, unsigned int line_number);
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
  */
-void monty_rot1(stack_rot1(stack_t **stack, unsigned int line_number))
+void monty_rotl(stack_t **stack, unsigned int line_number)
 {
 	stack_t *top, *bottom;
 
@@ -32,7 +32,7 @@ void monty_rot1(stack_rot1(stack_t **stack, unsigned int line_number))
 }
 
 /**
- * monty_rotr - Rotates the top value of a stack_t linked list to the top.
+ * monty_rotr - Rotates the bottom value of a stack_t linked list to the top.
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
  */
@@ -42,6 +42,7 @@ void monty_rotr(stack_t **stack, unsigned int line_number)
 
 	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
 		return;
+
 	top = (*stack)->next;
 	bottom = (*stack)->next;
 	while (bottom->next != NULL)
